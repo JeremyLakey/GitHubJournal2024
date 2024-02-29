39b7ad0e9f8ea2524a843c5a5553b5545a331841
personman61 <jeremysl123@gmail.com>
2024-02-28 21:45:13 -0700

    Actually solved this time. Leet code 1017.py

--- Problems/1017.py
+++ Problems/1017.py
@@ -1,39 +1,17 @@
-
 class Solution:
-
-    def helper(self, n: int) -> str:
-        if n == 3:
-            return "11"
-        if n == 2:
-            return "10"
-        if n == 1:
-            return "01"
-        if n == 0:
-            return "1"
-
-
     def baseNeg2(self, n: int) -> str:
-        # Base 4
-        # 0 === 100 = 4
-        # 3 === 111 = 3
-        # 2 === 110 = 2
-        # 1 === 001 = 1
-        # 0 === 000 = 0
-
-        if n == 4:
-            return "100"
-        if n == 3:
-            return "111"
-        if n == 2:
-            return "110"
         if n == 1:
             return "1"
         if n == 0:
             return "0"
 
-        t = ""
-        while n > 0:
-            m = n % 4
-            t = self.helper(n % 4) + t
-            n /= 4
+        r = n % (-2)
+        if r < 0:
+            r = 1 # abs of remainder 
+            n = n // (-2) + 1 # round towards zero instead of -inf
+        else:
+            n = n // (-2)
 
+
+
+        return self.baseNeg2(n) + str(r)
